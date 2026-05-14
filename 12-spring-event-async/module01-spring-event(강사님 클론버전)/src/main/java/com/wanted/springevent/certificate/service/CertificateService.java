33686498a14fb6c04157f98f5150d5bab14df82d
mnppi223 <mnppi223@gmail.com>
2026-05-14 11:07:03 +0900

    [STUDY] 2교시 학습 내용 정리
    section03 Transactional 받아올 것인가? 아니면 새로운 규칙인 Transactional을 받을 것인가.

--- 12-spring-event-async/module01-spring-event(강사님 클론버전)/src/main/java/com/wanted/springevent/certificate/service/CertificateService.java
+++ 12-spring-event-async/module01-spring-event(강사님 클론버전)/src/main/java/com/wanted/springevent/certificate/service/CertificateService.java
@@ -4,7 +4,7 @@ import com.wanted.springevent.certificate.entity.Certificate;
 import com.wanted.springevent.certificate.repository.CertificateRepository;
 import com.wanted.springevent.enrollment.entity.Enrollment;
 import com.wanted.springevent.enrollment.repository.EnrollmentRepository;
-import jakarta.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
@@ -22,6 +22,18 @@ public class CertificateService {
         this.enrollmentRepository = enrollmentRepository;
         this.certificateRepository = certificateRepository;
     }
+
+    /* comment.
+        Transactional 은 주요 속성들이 있다.
+        - propagation : 전파의 의미를 가지고 있고, default value 는
+        REQUIRED (상위 트렌젝션이 있다면 같이 묶인다.)
+        REQUIRED_NEW (상위 트렌젝션과 분리 된 독립 된 트렌젝션 경계를 만든다.)
+        - rollbackFor : 예외는 크게 2가지가 있다. 언체크 예외, 체크 예외
+        체크 예외 시에도 rollback 하기 위해서 작성해야 한다.
+        ex) rollbackFor = Exception.class
+        - timeout = 3 -> 3초 안에 통과해야한다.
+        너무 오래 걸리는 작업을 제한할 때 사용한다.
+     */
 
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void issueAfterCourseCompletion(Long enrollmentId, String courseTitle, String codePrefix) {
