943c5496bc180c97457abdc97438a70a5625238a
mnppi223 <mnppi223@gmail.com>
2026-05-18 12:40:11 +0900

    [STUDY] 5월 18일 학습 내용 정리

--- 12-spring-event-async/module02-spring-async/src/main/java/com/wanted/springasync/Module02SpringAsyncApplication.java
+++ 12-spring-event-async/module02-spring-async/src/main/java/com/wanted/springasync/Module02SpringAsyncApplication.java
@@ -2,7 +2,14 @@ package com.wanted.springasync;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+/* comment.
+    스케줄러 사용 가능하게 하는 어노테이션
+    작성하지 안으면, 동작을 아예 못함.
+ */
+
+@EnableScheduling
 @SpringBootApplication
 public class Module02SpringAsyncApplication {
 
